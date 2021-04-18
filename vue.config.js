5bafb89f4c322bf8b82aeabe416359e0c6072df1
Dima <dchaika1990@gmail.com>
2021-04-18 07:21:01 +0300

    add firebase

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,5 @@
 module.exports = {
 	css: {
-		// loaderOptions: {
-		// 	sass: {
-		// 		prependData: '@import "@/assets/scss/style.scss";'
-		// 	},
-		// },
 		sourceMap: process.env.NODE_ENV === 'development'
 	}
 }
